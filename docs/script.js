f4ae60656da2d9913a081226f6825d07910cfce9
Guaci Gutierrez <guacig@hotmail.com>
2021-09-23 18:53:29 +0100

    changes in all files

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,8 @@
-
 function myFunction() {
-    var x = document.getElementById("navigation");
-        if (x.className === "topbar") {
-            x.className += " responsive";
-        } 
-        else {
-            x.className = "topbar";
-        }
+  var x = document.getElementById("navigation");
+  if (x.className === "topbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "topbar";
+  }
 }
