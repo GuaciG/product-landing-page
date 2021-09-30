2854028dadcbd179bd5fb1c2f8717089060c0a92
Guaci Gutierrez <guacig@hotmail.com>
2021-09-30 17:59:30 +0100

    popup modal for images

--- docs/script.js
+++ docs/script.js
@@ -12,3 +12,37 @@ hamburgerBtn.onclick = function () {
   hamburgerBtn.classList.toggle("active");
   menuList.classList.toggle("active");
 };
+
+//Modal popup for images
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".img-gallery img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+const closeBtn = document.querySelector(".close-btn");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+    //change text and image
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = originalSrc;
+    const altText = preview.alt;
+    caption.textContent = altText;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
+
+closeBtn.addEventListener("click", (e) => {
+  if (e.target.classList.contains("close-btn")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
